81a7b4b8126e19eaf947072adbc96a5ea2e3144a
Deepak Kumar <dpk.41deep@gmail.com>
2025-04-01 19:03:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+    const uri = "https://api.github.com/users/deep-45deepak"
+    fetch(uri)
+        .then(response => response.json())
+        .then(data => {
+            const profileImage = document.getElementById('profile_image');
+            profileImage.setAttribute('src', data.avatar_url);
+        }
+        ).catch(error => {
+            console.error('Error fetching profile image');
+        }
+        );
+
     const toggleMode = document.querySelector("#darkMode");
 
     toggleMode.addEventListener("click", () => {
@@ -17,6 +30,31 @@ document.addEventListener("DOMContentLoaded", () => {
         mobileMenu.classList.toggle("hidden");
     });
 
+
+
+    const text = "A beautiful website starts here!";
+    let index = 0;
+    const speed = 150;
+    const textElement = document.getElementById("animated-text");
+
+    function animateText() {
+        if (index <= text.length) {
+            textElement.textContent = text.slice(0, index);
+            index++;
+            setTimeout(animateText, speed);
+        } else {
+            setTimeout(() => {
+                index = 0;
+                textElement.textContent = "";
+                animateText();
+            }, 1000);
+        }
+    }
+
+    animateText();
+
+
 });
+
 
 
